3efbadfaa06963b5faf511027f4b6af7265e2405
SMLkaiellis08 <65583319+SMLkaiellis08@users.noreply.github.com>
2020-08-14 04:52:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,198 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+const token = ('NzM4ODU5Mjc5MjU1MzM5MDkw.XySCUw.XRPg0SDfPqyHd2dCqWSTQDeFfqs');
+const prefix = "!"
+const ytdl = require("ytdl-core");
+
+let servers = {};
+
+bot.on('ready', () =>{
+console.log('conected')
+})
+
+bot.on('message',msg=>{
+    if(msg.content === "!!info"){
+        msg.reply('info commands are !!info version, !!info ath')
+        msg.reply('remember this bot is in beta please contact @smlkaiellis08#0922 if you have any sugestions')
+bot.on('ready', () =>{
+console.log('ath')
+})
+
+    }
+    if(msg.content === "!!info ath"){
+        msg.reply('ath = SMLkaiellis08')
+        msg.reply('email = kaicycle11@gmail.com')
+        msg.reply('server = https://discord.gg/pD2Ubb2')
+bot.on('ready', () =>{
+console.log('ath')
+})
+    
+}
+
+if(msg.content === "!!info version"){
+    msg.reply('mod-bot version = 0.0.5')
+    msg.reply('0.0.5 is in beta right now')
+    }
+    if(msg.content === "Bastard"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "!!playlist all"){
+        msg.reply('at this point of time this command is not yet avalible for public D:')
+    }
+    if(msg.content === "Dick"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "Dickhead"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "Cock"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "?p"){
+        msg.delete()
+        msg.delete()
+        msg.delete()
+    }
+    if(msg.content === "!!test"){
+       msg.reply('i will do TEST')
+       msg.reply('test')
+       msg.reply('test')
+       msg.reply('test')
+       msg.reply('test')
+       msg.reply('test')
+       msg.reply('test')
+
+bot.on('ready', () =>{
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+console.log('test')
+})
+
+    }
+    if(msg.content === "fuck"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+bot.on('ready', () =>{
+console.log('message deleted')
+})
+
+    }
+    if(msg.content === "Fuck"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "Motherfucker"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "motherfucker"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "MotherFucker"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "Fuck you"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "fuck you"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "Fuck You"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "fu"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "FU"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "f u"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "F U"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "F.U"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+    if(msg.content === "f.u"){
+        msg.delete()
+        msg.reply('please do not say that')
+        msg.delete()
+
+    }
+})
+
+
+
+
+bot.login(token);
